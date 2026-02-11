1d9930897ea855a1ce874c57848886c420bb690e
AD <adem.arfaoui2025@gmail.com>
2026-02-11 19:28:16 +0100

    Update organisateurs.jsx

--- src/components/Main-screen/home/organisateurs/organisateurs.jsx
+++ src/components/Main-screen/home/organisateurs/organisateurs.jsx
@@ -4,28 +4,7 @@ import "./organisateurs.css";
 // Tu ajoutes / modifies les logos ici
 
 const Organisateurs = forwardRef((props, ref) => {
-  const organisateursData = [
-    {
-      name: "Apecialized Arab Women Association",
-      logo: "/images & logos/afas-logo.jpg",
-      website: "https://www.facebook.com/profile.php?id=100072063352431",
-      categorie: "Association",
-    },
-    {
-      name: "Club Ambassadeurs Vesos TN | FSEGT",
-      logo: "/images & logos/sos-logo.png",
-      website:
-        "https://www.instagram.com/clubs.ambassadeurs.sos.village?igsh=YndieGl4dmRsbGgz",
-      categorie: "Association",
-      id: "sos",
-    },
-    {
-      name: "Association tunisienne pour les objectifs de développement durable",
-      logo: "/images & logos/ATODD.png",
-      website: "https://atodd.tn",
-      categorie: "Association",
-    },
-  ];
+
 
   return (
     <section
@@ -51,25 +30,23 @@ const Organisateurs = forwardRef((props, ref) => {
         </div>
       </div>
 
-      <div style={{ marginTop: "100px" }}>
-        <h2 className='organisateurs-title'>Associations</h2>
-        <p className='organisateurs-subtitle'>
-          Associations participant au Job Fair 2026
-        </p>
+      <div>
+        <h2 className='organisateurs-title'>Partenaire</h2>
+        <p className='organisateurs-subtitle'>Partenaire du Job Fair 2026</p>
         <div className='organisateurs-grid' style={{ marginBottom: "50px" }}>
-          {organisateursData.map((organisateur) => (
-            <a
-              href={organisateur.website}
-              target='_blank'
-              className='organisateur-card'
-            >
-              <img src={organisateur.logo} alt='logo' id={organisateur.id} />
-              <h3>{organisateur.name}</h3>
-              <span>{organisateur.categorie}</span>
-            </a>
-          ))}
+          <a
+            href='https://www.facebook.com/4CFSEGT'
+            target='_blank'
+            className='organisateur-card'
+            id='org'
+          >
+            <img src='/images & logos/4c.jpg' alt='logo' />
+            <h3>Direction des Stages</h3>
+            <span>Organisateur</span>
+          </a>
         </div>
       </div>
+      
     </section>
   );
 });
